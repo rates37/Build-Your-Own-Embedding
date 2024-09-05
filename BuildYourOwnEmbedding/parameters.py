7ecc78d52b2d36a086bdfac88246b5888e0e92c9
rates37 <satyajhaveri@protonmail.ch>
2024-09-05 18:27:28 +1000

    Update Parameter documentation

--- BuildYourOwnEmbedding/parameters.py
+++ BuildYourOwnEmbedding/parameters.py
@@ -31,6 +31,11 @@ class Parameter(ABC):
         pass
 
     def __len__(self) -> int:
+        """Gets the number of values that this parameter can take on.
+
+        Returns:
+            int: The number of values that the Parameter will take.
+        """
         return self.numSamples
 
 
@@ -42,6 +47,16 @@ class UniformRangeParameter(Parameter):
     def __init__(
         self, minValue: np.number, maxValue: np.number, numSamples: int
     ) -> None:
+        """Constructor for UniformRangeParameter class.
+        
+        This Parameter takes on a uniform range of values between `minValue` and 
+        `maxValue`, with `numSamples` evenly spaced points.
+
+        Args:
+            minValue (np.number): The minimum value that the parameter can take on
+            maxValue (np.number): The maximum value that the parameter can take on
+            numSamples (int): The number of different values that the parameter can take on
+        """
         super().__init__(numSamples)
         self.minValue: np.number = minValue
         self.maxValue: np.number = maxValue
@@ -69,6 +84,16 @@ class RandomRangeParameter(Parameter):
     def __init__(
         self, minValue: np.number, maxValue: np.number, numSamples: int
     ) -> None:
+        """Constructor for the RandomRangeParameter class.
+        
+        This Parameter takes on a random sample of `numSamples` values between 
+        `minValue` and `maxValue`.
+
+        Args:
+            minValue (np.number): The minimum value that the parameter can take on
+            maxValue (np.number): The maximum value that the parameter can take on
+            numSamples (int): The number of different values that the parameter can take on
+        """
         super().__init__(numSamples)
         self.minValue: np.number = minValue
         self.maxValue: np.number = maxValue
@@ -98,6 +123,16 @@ class FixedParameterSet(Parameter):
         values: Union[npt.NDArray, List[np.number]],
         dtype: npt.DTypeLike = np.float64,
     ) -> None:
+        """Constructor for the FixedParameterSet class.
+        
+        This parameter takes on a specific sequence of values.
+
+        Args:
+            values (Union[npt.NDArray, List[np.number]]): A list or array 
+                containing a sequence of values that the parameter should take on
+            dtype (npt.DTypeLike, optional): The data type that the values should 
+                be returned as. Defaults to np.float64.
+        """
         super().__init__(len(values))
         self.values = np.array(values, dtype=dtype)
 
@@ -119,6 +154,16 @@ class LogRangeParameter(Parameter):
     def __init__(
         self, minValue: np.number, maxValue: np.number, numSamples: int
     ) -> None:
+        """Constructor for the LogRangeParameter class.
+        
+        This Parameter takes on a range of `numSamples` values logarithmically 
+        spaced between `minValue` and `maxValue`.
+
+        Args:
+            minValue (np.number): The minimum value that the parameter can take on
+            maxValue (np.number): The maximum value that the parameter can take on
+            numSamples (int): The number of different values that the parameter can take on
+        """
         super().__init__(numSamples)
         self.minValue: np.number = minValue
         self.maxValue: np.number = maxValue
@@ -144,6 +189,13 @@ class ConstantParameter(Parameter):
     """
 
     def __init__(self, value: np.number) -> None:
+        """Constructor for the ConstantParameter class.
+        
+        This Parameter always only ever takes on a single value.
+
+        Args:
+            value (np.number): The value that this parameter will take on.
+        """
         super().__init__(1)  # only ever 1 value
         self.value: np.number = value
 
