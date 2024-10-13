77804fe9758a1e6f04c412ef60aef39e7b72b98a
Satya Jhaveri <satyajhaveri@protonmail.ch>
2024-10-13 22:38:30 +1100

    Refactored TestParameter class methods

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -13,44 +13,34 @@ EPSILON = 1e-6
 
 class TestParameters(unittest.TestCase):
 
-    def test_uniform_range_parameter(self) -> None:
-
-        # Test 1:
+    # UniformRangeParameter tests:
+    def test_uniform_range_parameter_simple(self) -> None:
         minVal, maxVal = 0, 1
         numSamples = 5
-        expectedRange = [minVal, maxVal]
         expectedValues = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
-
         param = UniformRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
-        self.assertTrue(all(abs(values - expectedValues) < EPSILON))
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
+        self.assertTrue(np.allclose(values, expectedValues, atol=EPSILON))
 
-        # Test 2:
+    def test_uniform_range_parameter_negative_values(self) -> None:
         minVal, maxVal = -1, 1
         numSamples = 9
-        expectedRange = [minVal, maxVal]
         expectedValues = np.array([-1.0, -0.75, -0.5, -0.25, 0.0, 0.25, 0.5, 0.75, 1.0])
-
         param = UniformRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
-        self.assertTrue(all(abs(values - expectedValues) < EPSILON))
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
+        self.assertTrue(np.allclose(values, expectedValues, atol=EPSILON))
 
-        # Test 3:
+    def test_uniform_range_parameter_non_integer_values(self) -> None:
         minVal, maxVal = -24, -22
         numSamples = 8
-        expectedRange = [minVal, maxVal]
         expectedValues = np.array(
             [
                 -24.0,
@@ -63,135 +53,116 @@ class TestParameters(unittest.TestCase):
                 -22.0,
             ]
         )
-
         param = UniformRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
-        self.assertTrue(all(abs(values - expectedValues) < EPSILON))
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
+        self.assertTrue(np.allclose(values, expectedValues, atol=EPSILON))
 
-    def test_random_range_parameter(self) -> None:
-
-        # Test 1:
+    # RandomRangeParameter tests:
+    def test_random_range_parameter_basic(self) -> None:
         minVal, maxVal = 0, 1
         numSamples = 5
-        expectedRange = [minVal, maxVal]
         param = RandomRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
 
-        # Test 2:
+    def test_random_range_parameter_large_range(self) -> None:
         minVal, maxVal = -20, 20
         numSamples = 40
-        expectedRange = [minVal, maxVal]
         param = RandomRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
 
-        # Test 3:
-        minVal, maxVal = 37, 1200
+    def test_random_range_parameter_single_value(self) -> None:
+        minVal, maxVal = 100, 100
+        numSamples = 5
+        param = RandomRangeParameter(
+            minValue=minVal, maxValue=maxVal, numSamples=numSamples
+        )
+        values = param.get_values()
+        self.assertTrue(np.all(values == 100))
+        self.assertEqual(len(values), numSamples)
+
+    def test_random_range_parameter_negative_range(self) -> None:
+        minVal, maxVal = -1200, -37
         numSamples = 123
-        expectedRange = [minVal, maxVal]
         param = RandomRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
 
-    def test_constant_parameter(self) -> None:
-        # Test 1:
+    # ConstantParameter tests:
+    def test_constant_parameter_single_value(self) -> None:
         value = 0.5
         param = ConstantParameter(value=value)
         values = param.get_values()
         self.assertEqual(len(values), 1)
         self.assertEqual(values[0], value)
 
-        # Test 2:
+    def test_constant_parameter_negative_value(self) -> None:
         value = -5
         param = ConstantParameter(value=value)
         values = param.get_values()
         self.assertEqual(len(values), 1)
         self.assertEqual(values[0], value)
 
-        # Test 3:
+    def test_constant_parameter_large_value(self) -> None:
         value = 45
         param = ConstantParameter(value=value)
         values = param.get_values()
         self.assertEqual(len(values), 1)
         self.assertEqual(values[0], value)
 
-    def test_fixed_parameter(self) -> None:
-
-        # Test 1:
+    # FixedParameter tests:
+    def test_fixed_parameter_set_basic(self) -> None:
         originalValues = [1, 2, 3, 4]
         param = FixedParameterSet(values=originalValues)
         values = param.get_values()
         self.assertEqual(len(values), len(originalValues))
-        self.assertTrue(sum(abs(np.array(values) - np.array(originalValues))) < EPSILON)
+        self.assertTrue(np.allclose(values, originalValues, atol=EPSILON))
 
-        # Test 2:
-        originalValues = np.array([1, 2, 3, 4])
+    def test_fixed_parameter_set_float_values(self) -> None:
+        originalValues = np.array([1.1, 2.2, 3.3, 4.4])
         param = FixedParameterSet(values=originalValues)
         values = param.get_values()
         self.assertEqual(len(values), len(originalValues))
-        self.assertTrue(all(abs(np.array(values) - np.array(originalValues))) < EPSILON)
+        self.assertTrue(np.allclose(values, originalValues, atol=EPSILON))
 
-        # Test 3:
-        originalValues = [
-            0.43209799,
-            0.61502947,
-            0.01217385,
-            0.84063715,
-            0.14213936,
-            0.21388344,
-            0.41052122,
-            0.74804918,
-            0.11104376,
-        ]
+    def test_fixed_parameter_set_large_set(self) -> None:
+        originalValues = np.random.rand(1000)
         param = FixedParameterSet(values=originalValues)
         values = param.get_values()
         self.assertEqual(len(values), len(originalValues))
-        self.assertTrue(all(abs(np.array(values) - np.array(originalValues))) < EPSILON)
+        self.assertTrue(np.allclose(values, originalValues, atol=EPSILON))
 
-    def test_log_range_parameter(self) -> None:
-
-        # Test 1:
+    # LogRangeParameter tests:
+    def test_log_range_parameter_basic(self):
         minVal, maxVal = 1, 10
         numSamples = 5
-        expectedRange = [minVal, maxVal]
         expectedValues = np.array([1.0, 1.77827941, 3.16227766, 5.62341325, 10.0])
-
         param = LogRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
-        )
-        self.assertTrue(all(abs(values - expectedValues) < EPSILON))
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
+        self.assertTrue(np.allclose(values, expectedValues, atol=EPSILON))
 
-        # Test 2:
+    def test_log_range_parameter_large_range(self):
+        # Test logarithmic range with a large range of values
         minVal, maxVal = 1, 1e9
         numSamples = 10
-        expectedRange = [minVal, maxVal]
         expectedValues = np.array(
             [
                 1.0e0,
@@ -206,16 +177,26 @@ class TestParameters(unittest.TestCase):
                 1.0e9,
             ]
         )
-
         param = LogRangeParameter(
             minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
         values = param.get_values()
         self.assertEqual(len(values), numSamples)
-        self.assertTrue(
-            np.all(values >= expectedRange[0]) and np.all(values <= expectedRange[1])
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
+        self.assertTrue(np.allclose(values, expectedValues, atol=EPSILON))
+
+    def test_log_range_parameter_small_log_range(self):
+        # Test logarithmic range with a narrow range of values
+        minVal, maxVal = 1, 10
+        numSamples = 5
+        expectedValues = np.array([1.0, 1.77827941, 3.16227766, 5.62341325, 10.0])
+        param = LogRangeParameter(
+            minValue=minVal, maxValue=maxVal, numSamples=numSamples
         )
-        self.assertTrue(all(abs(values - expectedValues) < EPSILON))
+        values = param.get_values()
+        self.assertEqual(len(values), numSamples)
+        self.assertTrue(np.all((values >= minVal) & (values <= maxVal)))
+        self.assertTrue(np.allclose(values, expectedValues, atol=EPSILON))
 
 
 if __name__ == "__main__":
