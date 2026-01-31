ccab550d15c94cc52ea7b805f01f197b4248aeea
Satya Jhaveri <satyajhaveri@protonmail.ch>
2026-01-31 14:26:30 +1100

    Update main module __init__

--- BuildYourOwnEmbedding/__init__.py
+++ BuildYourOwnEmbedding/__init__.py
@@ -1,3 +1,22 @@
-from . import parameters
-from . import responses
-from . import functional
+"""
+BuildYourOwnEmbedding: A library for generating and analysing synthetic neural responses
+
+This library provides tools for:
+    * Modelling various neural response functions
+    * Generating sets of synthetic neural responses with configurable parameters
+    * Adding realistic noise to responses
+    * Computing and visualising RDMs
+    * Dimensionality reduction techniques
+    * Information-theoretic measures
+"""
+
+__version__ = "2.0.0"
+
+from BuildYourOwnEmbedding import analysis, metrics, parameters, responses
+
+__all__ = [
+    "parameters",
+    "responses",
+    "metrics",
+    "analysis",
+]
